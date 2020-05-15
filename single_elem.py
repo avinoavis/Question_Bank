2611cf7efcd6fdf374c261c715e2ce76d3afc62c
avinoavis <54793990+avinoavis@users.noreply.github.com>
2020-05-15 22:37:57 +0530

    Add files via upload

--- single_elem.py
+++ single_elem.py
@@ -9,23 +9,21 @@ def sin_ele(a):
         indx =  len(a)//2 
         k = a[indx]
         if k == a[indx-1]:
-            l = a[:indx-1]
-            ll = a[indx+1:]
-            if len(l)%2==0:
-                return sin_ele(ll)
+            a1,a2 = a[:indx-1],a[indx+1:]
+            if len(a1)%2==0:
+                return sin_ele(a2)
             else:
-                return sin_ele(l)
+                return sin_ele(a1)
         elif k == a[indx+1]:
-            l = a[:indx]
-            ll = a[indx+2:]
-            if len(l)%2==0:
-                return sin_ele(ll)
+            a1,a2 = a[:indx],a[indx+2:]
+            if len(a1)%2==0:
+                return sin_ele(a2)
             else:
-                return sin_ele(l)
+                return sin_ele(a1)
         else:
             return k
             
 # U can change the values of array for more test cases            
-a = [1,1,2,2,3,3,4,4,5]
+a = [1,1,2,2,3,3,4,4,5,6,6]
 ans = sin_ele(a)
 print(ans)
