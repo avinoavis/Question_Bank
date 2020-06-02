5b85c4c52fa9c7c1ed72b24460e0f8976b77fa73
avinoavis <54793990+avinoavis@users.noreply.github.com>
2020-06-02 17:39:25 +0530

    Add files via upload

---
+++ spiral_matrix.py
@@ -0,0 +1,34 @@
+def fun(arr,m,n):
+    i,j =0,0
+    for k in range((n+1)//2):
+        for a in range(n-1):
+            print(arr[i][j])
+            j+=1
+        for a in range(m-1):
+            print(arr[i][j])
+            i+=1
+        for a in range(n-1):
+            print(arr[i][j])
+            j-=1
+        for a in range(m-1):
+            print(arr[i][j])
+            i-=1
+        i+=1
+        j+=1
+        m-=2
+        n-=2
+        if m==1 and n==1:
+            print(arr[i][j])
+            return
+        if n==1:
+            for a in range(m):
+                print(arr[i][j])
+                i+=1
+            return
+        if m==1:
+            for a in range(n):
+                print(arr[i][j])
+                j+=1
+            return
+a = [[1,2,3,4,5] for i in range(5)]
+fun(a,5,5)
