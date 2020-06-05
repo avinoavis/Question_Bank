caf5ab780fb346c48e6003178e6f9f957decf633
avinoavis <54793990+avinoavis@users.noreply.github.com>
2020-06-05 22:35:57 +0530

    Update Last_one_remaining.py

--- Last_one_remaining.py
+++ Last_one_remaining.py
@@ -1,7 +1,6 @@
 def last_one(a):
     x =0
     while len(a)>1:
-        
         if x+1 == len(a)-1:
             a.remove(a[x+1])
             x = 0
@@ -11,7 +10,6 @@ def last_one(a):
         else:
             a.remove((a[x+1]))
             x +=1
-        
     return a
 
 
